3370827af7ed534f57ddb8864ed6c38f3ce41b9c
danieljvalperga <59209569+danieljvalperga@users.noreply.github.com>
2020-02-03 17:04:00 -0800

    Add binary search

---
+++ BinarySearch.js
@@ -0,0 +1,25 @@
+let arr = [1, 3, 5, 7, 8, 9]
+let start = 0
+let end = arr.length
+
+function search(array, value, start, end) {
+	if (start < 0 || end > array.length) {
+		return "Invalid value start[" + start + "] end[" + end + "] for array length[" + array.length + "]"
+	}
+
+	let mid = Math.floor((start + end) / 2)
+
+	console.log("value[" + value + "] mid[" + mid + "] arraymid[" + array[mid] + "] start[" + start + "] end[" + end + "]")
+
+	if (array[mid] === value) {
+		return "Element found!"
+	} else if ((end - start) <= 0) {
+		return "Element not found!"
+	} else if (array[mid] > value) {
+		return search(array, value, start, (mid - 1))
+	} else {
+		return search(array, value, (mid + 1), end)
+	}
+}
+
+console.log(search(arr, 5, 0, arr.length))
